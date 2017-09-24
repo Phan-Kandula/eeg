6c900dfcc613d35fe48c2e436d0b3ad65a4b547d
phani1615 <phanindra.kandula@gmail.com>
2017-09-24 15:00:16 -0700

    added file writing support

--- datahandling.py
+++ datahandling.py
@@ -3,16 +3,15 @@ import numpy as np
 import serial
 
 ser = serial.Serial("COM7", 9600, timeout=None)
-#ser.port = input("Port(COM_): ")
-#ser.baudrate = input("baudrate(9600):")
 ser.timeout = None
 
 line = ""
+f = file("data2.csv", "a+")
 
 while (True):
     c = ser.read()
     line = line + c.decode("utf-8")
     if c == b'\n':
         print(line)
+        f.write(line)
         line = ""
-    #print (str(c))
