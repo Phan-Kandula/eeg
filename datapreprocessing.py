77c89db58da849f08a7a5fda3c6f494e0374cab5
phani1615 <phanindra.kandula@gmail.com>
2017-09-26 22:19:18 -0700

    more functionality

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -7,22 +7,8 @@ from pandas import DataFrame
 dataframe = pd.read_csv("data.csv")
 inputX = dataframe.iloc[:, 1:].values
 inputY = dataframe.iloc[:, 0]
-
-print(type(inputY))
 LabelEncoderY = LabelEncoder()
 inputY = LabelEncoderY.fit_transform(inputY)
-'''
-ohc = OneHotEncoder(categorical_features=[0])
-inputY = ohc.fit_transform(inputY)
-'''
 inputY = np.reshape(inputY, (inputY.size, 1))
-print(type(inputY))
-'''
-inputY = DataFrame(inputY)
-print(type(inputY))
-'''
-print(type(inputY))
-print(inputY)
 ohenc = OneHotEncoder(categorical_features="all")
 inputY = ohenc.fit_transform(inputY).toarray()
-print(inputY)
